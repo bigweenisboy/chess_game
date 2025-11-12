51ab6341136ad242785520d10ea2d8d3e67a2f74
bigweenisboy <barracloughleonard@gmail.com>
2025-11-12 09:12:40 +0000

    Update board.py

--- board.py
+++ board.py
@@ -1,1 +1,4 @@
-
+# Board is a 8x8 matrix, each cell is either empty or stores a piece.
+# Base class "Piece" with attributes: colour, position
+# Subclass includes: Pawn, Rook, Knight, Bishop, Queen, KIng
+# Each subclass implements valid_moves(board)
